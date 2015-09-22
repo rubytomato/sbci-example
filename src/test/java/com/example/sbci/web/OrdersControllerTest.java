807a95bbadc0fc7d8f782c3a822fe85dcf96334d
rubytomato <vd4s-wtnb@asahi-net.or.jp>
2015-09-22 23:03:33 +0900

    jmxテストのデバッグ

--- src/test/java/com/example/sbci/web/OrdersControllerTest.java
+++ src/test/java/com/example/sbci/web/OrdersControllerTest.java
@@ -1,20 +1,10 @@
 package com.example.sbci.web;
 
 import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.lang.management.ManagementFactory;
-import java.util.List;
-import java.util.Set;
-
-import javax.management.MBeanServer;
-import javax.management.MBeanServerFactory;
-import javax.management.ObjectInstance;
-import javax.management.ObjectName;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -59,32 +49,6 @@ public class OrdersControllerTest {
     .andExpect(status().isOk())
     .andExpect(content().contentType("text/html;charset=UTF-8"))
     .andExpect(content().string(containsString("<h3>detail - contents</h3>")));
-  }
-
-  //org.apache.tomcat.jdbc.pool.jmx:name=dataSourceMBean,type=ConnectionPool
-  //com.example.sbci:type=ConnectionPool,*
-  public static final String OBJ_NAME = "org.apache.tomcat.jdbc.pool.jmx:name=dataSourceMBean,type=ConnectionPool";
-
-  @Test
-  public void testJmx() throws Exception {
-    System.out.println("***testJmx***");
-    List<MBeanServer> mbservers = MBeanServerFactory.findMBeanServer(null);
-    if (mbservers.size() > 0) {
-      for (MBeanServer s : mbservers) {
-        System.out.println("[[" + s.getDefaultDomain() + "]]");
-      }
-    }
-
-    System.out.println("***");
-    Set<ObjectInstance> obj = ManagementFactory.getPlatformMBeanServer().queryMBeans(new ObjectName(OBJ_NAME), null);
-    System.out.println("[[size:" + obj.size() + "]]");
-    obj.forEach(a -> {
-      System.out.println("className:" + a.getClassName());
-      System.out.println("canonicalName:" + a.getObjectName().getCanonicalName());
-    });
-
-    //assertThat(ManagementFactory.getPlatformMBeanServer().queryMBeans(new ObjectName(OBJ_NAME), null).size(), is(1));
-    assertThat("jmxTest", true);
   }
 
 }
